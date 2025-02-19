5cafa6171b35e6f98da93da59bee793ea1941872
Juan Lluviera <juanlluviera03@gmail.com>
2025-02-19 09:07:10 -0300

    initial

---
+++ guardianes-del-entorno/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HeaderGuardianes from './Components/headerGuardianes.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <HeaderGuardianes />
+    </div>
+  );
+}
+
+export default App;
---
+++ guardianes-del-entorno/src/Components/headerGuardianes.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../App.css'
+
+export default function HeaderGuardianes() {
+  return (
+  <div>
+        <header className="container-fluid header bg-dkgreen text-white text-center py-5">
+            <div className="row">
+                <div className="col-12">
+                    <h1>Guardianes del Entorno</h1>
+                    <p >¡Cuidando nuestro planeta!</p>
+                </div>
+            </div>        
+        </header>
+   </div>  
+  )}
