14fc9adcbd6cb1dc0009db32abb6c3c65df7343b
Juan Lluviera <juanlluviera03@gmail.com>
2025-02-19 16:15:23 -0300

    arreglado redux

--- guardianes-del-entorno/src/Components/headerGuardianes.jsx
+++ guardianes-del-entorno/src/Components/headerGuardianes.jsx
@@ -5,8 +5,8 @@ import Register from './Register'
 export default function HeaderGuardianes() {
   return (
   <div>
-        <header className="container-fluid header bg-dark text-white text-center py-5">
-            <nav>
+        <header className="container-fluid header">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="row in-line-flex">
                     <div className="col-12">
                         <h1>Guardianes del Entorno</h1>
