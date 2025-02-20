91d01a07e190813088040a08b5099aeb9ef38b4e
Juan Lluviera <juanlluviera03@gmail.com>
2025-02-20 20:21:51 -0300

    Viewarea con prueba

--- guardianes-del-entorno/src/App.js
+++ guardianes-del-entorno/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import HeaderGuardianes from './Components/headerGuardianes.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import ViewAreas from './Components/ViewAreas.jsx';
 
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
       <div>
       <HeaderGuardianes />
       </div>     
-
+      <div className="container">
+        <ViewAreas />
+      </div>
     </div>
   );
 }
