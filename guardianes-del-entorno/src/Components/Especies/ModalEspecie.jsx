6970f6a647f7798a1a0568c18a9f65be81ac3e53
Juan Lluviera <juanlluviera03@gmail.com>
2025-03-07 00:06:12 -0300

    ultimo yaya

--- guardianes-del-entorno/src/Components/Especies/ModalEspecie.jsx
+++ guardianes-del-entorno/src/Components/Especies/ModalEspecie.jsx
@@ -8,16 +8,18 @@ const ModalEspecie = ({ especie, onClose }) => {
    const user = useSelector((state) => state.user);
   return (
     <div className='modal-backdrop'>
-      <div className='modal-content'>
-        <button className='modal-close' onClick={onClose}>✖</button>
-        <h2>{especie.commonName}</h2>
-        <ul>
-          <li><strong>Nombre Científico:</strong> {especie.scientificName}</li>
-          <li><strong>Categoría:</strong> {especie.category}</li>
-          <li><strong>Estado de Conservación:</strong> {especie.conservationStatus}</li>
-          <li><ShowComment entityId={especie.id} entityType={"species"} /> </li>
-          <li><CommentInput userId={user.id} speciesId={especie.id} /></li>
-        </ul>
+      <div className='modal-dialog modal-dialog-scrolleable'>
+        <div className='modal-content'>
+          <button className='modal-close' onClick={onClose}>✖</button>
+          <h2>{especie.commonName}</h2>
+          <ul>
+            <li><strong>Nombre Científico:</strong> {especie.scientificName}</li>
+            <li><strong>Categoría:</strong> {especie.category}</li>
+            <li><strong>Estado de Conservación:</strong> {especie.conservationStatus}</li>
+            <li><ShowComment entityId={especie.id} entityType={"species"} /> </li>
+            <li><CommentInput userId={user.id} speciesId={especie.id} /></li>
+          </ul>
+        </div>
       </div>
     </div>
   );
